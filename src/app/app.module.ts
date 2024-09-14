da9cacddfe42c0cf8abf190c3f58050dc9f492ab
VICTOR SANCHEZ <victorssanchez@hotmail.es>
2024-09-14 13:27:50 +0200

    implementado el buscador de heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
 
     AppRoutingModule,
     SharedModule
--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+
+  public hero? : Hero;
+
+  constructor(
+    private heroesServices:  HeroesService,
+    private activatedRouter: ActivatedRoute,
+    private router:          Router
+  ) { }
+
+
+  ngOnInit(): void {
+    this.activatedRouter.params
+      .pipe(
+        switchMap(({ id }) => this.heroesServices.getHeroesById(id)),
+      ).subscribe(
+        hero => {
+          if (!hero) return this.router.navigate(['heroes/list']);
+
+          this.hero = hero;
+          return;
+        })
+  }
+
+  goBack(){
+    this.router.navigateByUrl('heroes/list');
+  }
 
 }
